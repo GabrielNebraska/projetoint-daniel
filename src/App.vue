db3a675365b451820e52406de4a044343488364e
sentido1337 <abusentidoramaldavida@gmail.com>
2023-06-21 15:05:55 -0300

    projetodaniel

---
+++ src/App.vue
@@ -0,0 +1,134 @@
+<script setup></script>
+
+<template>
+ 
+
+ <div class="buttons">
+
+  <button class="quizhist">Acessar Quiz</button>
+  <button class="quizmat">Acessar Quiz</button>
+  <button class="quizport">Acessar Quiz</button>
+  <button class="quizgeo">Acessar Quiz</button>
+
+ </div>
+
+  <div class="historia">
+    História <br /><br />
+
+    A história estuda as mudanças e <br />
+    permanências ocorridas na sociedade. <br />
+    Procura perceber o modo como às <br />pessoas viviam nos tempos antigos e como <br />
+    vivem hoje, bem como a relação <br />entre aqueles tempos e os tempos atuais. <br />
+    Ou seja, a história estuda o <br />tempo passado e também o presente. <br />
+    Nesse Quiz, venha textar sua habilidade <br />histórica e aprender mais sobre a humanidade!
+
+    <br />
+    <br />
+    <button class="quizhist">Acessar Quiz</button>
+    <br>
+  </div>
+
+  <div class="matematica">
+    Matemática <br />
+    <br />
+
+    studa números e operações,<br />formas geométricas, medidas, <br />
+    interpretação de dados, estudo <br />de variação, raciocínio lógico, <br />
+    entre outros. Suas principais <br />áreas de estudo são a aritmética, <br />
+    álgebra, geometria e estatística. <br />
+    Nesse Quiz o seu gênio interior será testado, <br />
+    encare este desafio matemático!
+
+    <br />
+    <br />
+    <button class="quizmat">Acessar Quiz</button>
+    <br>
+  </div>
+
+  <div class="portugues">
+    Português <br />
+    <br />
+
+    Dentre as áreas mais importantes da disciplina, <br />
+    estão: morfologia, que estuda as palavras isoladamente; <br />
+    sintaxe, responsável por estudar<br />
+    as palavras enquanto elementos <br />
+    de uma frase; e semântica, que estuda o significado<br />
+    das palavras e os sentidos que elas<br />
+    podem tomar de acordo com o contexto. <br />
+    Nesse Quiz seu raciocínio será textado, <br />vamos ver se você sabe mesmo interpretar! <br />
+    <br />
+    <button class="quizport">Acessar Quiz</button>
+    <br>
+  </div>
+
+  <div class="geografia">
+    Geografia <br />
+    <br />
+
+    O que é GEOGRAFIA? Do relevo, clima e vegetação <br />
+    ao estudo de populações, a GEOGRAFIA <br />
+    estuda o espaço territorial como um todo. <br />
+    Disciplina abrangente, a GEOGRAFIA preocupa-se com as<br />
+    modificações sociais e físicas das regiões, pois considera <br />
+    tanto as transformações da natureza quanto do <br />
+    homem no ambiente e nas paisagens.
+    <br />
+    Venha nesse Quiz descobrir suas habilidades geográficas! <br />
+    vamos ver o quanto você sabe do mundo!
+
+    <br />
+    <br />
+    <button class="quizgeo">Acessar Quiz</button>
+    <br>
+  </div>
+</template>
+
+<style scoped>
+.quizgeo {
+  background-color: rgb(247, 55, 21);
+}
+
+.quizport {
+  background-color: rgb(137, 39, 223);
+}
+
+.quizmat {
+  background-color: rgb(29, 51, 243);
+}
+.quizhist {
+  background-color: rgb(255, 0, 0);
+}
+
+.titulo {
+  text-align: center;
+}
+.historia {
+  background-color: rgb(105, 8, 8);
+  color: white;
+  text-align: center;
+  font-size: large;
+  border-radius: 2%;
+}
+.matematica {
+  background-color: rgb(2, 2, 105);
+  color: white;
+  text-align: center;
+  font-size: large;
+  border-radius: 2%;
+}
+.portugues {
+  background-color: rgb(74, 14, 129);
+  color: white;
+  text-align: center;
+  font-size: large;
+  border-radius: 2%;
+}
+.geografia {
+  background-color: rgb(99, 26, 26);
+  color: white;
+  text-align: center;
+  font-size: large;
+  border-radius: 2%;
+}
+</style>
